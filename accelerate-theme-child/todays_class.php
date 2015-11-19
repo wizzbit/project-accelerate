699ef2950146ce4d1327209e9884142d3dd61c75
Nathan Barclay <nathanbarclay1@Nathans-MacBook-Pro.local>
2015-11-19 16:47:43 +0000

    Accel Project

---
+++ accelerate-theme-child/todays_class.php
@@ -0,0 +1,19 @@
+<aside class="case-study-images">
+
+<h2>MailChimp Campaign</h2> 
+
+<h5>Content Strategy, Design & Development</h5>
+<h6>Client: MailChimp</h6>
+
+<p>Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! ! Here is a copy of what I did in today's lesson ! ! !</p>
+</aside>
+
+<div class="case-study-images">
+</div>
+
+
+<img src="http://localhost:8888/accelerate/wp-content/uploads/2015/11/mc-thumbnail-3-512x1024.png" alt="mc-thumbnail-3" width="512" height="1024" class="alignnone size-large wp-image-47335" />
+
+<img src="http://localhost:8888/accelerate/wp-content/uploads/2015/11/mc-thumbnail-2.png" alt="mc-thumbnail-2" width="606" height="817" class="alignnone size-full wp-image-47334" />
+
+<img src="http://localhost:8888/accelerate/wp-content/uploads/2015/11/mc-thumbnail-1.png" alt="mc-thumbnail-1" width="608" height="448" class="alignnone size-full wp-image-47333" /><h2>MailChimp Campaign</h2>
